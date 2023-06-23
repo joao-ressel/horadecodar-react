d64330428a96c300ef7cb968a21ac770016cd2bd
joao-ressel <joao.resselti@gmail.com>
2023-06-23 09:26:17 -0500

    Avançando em props

--- src/components/List.js
+++ src/components/List.js
@@ -4,9 +4,10 @@ function List(){
         <>
             <h1>Minha Lista</h1>
             <ul>
-                <Item marca="Ferrari"/>
-                <Item marca="Fiat"/>
-                <Item marca="Wolksvagen"/>
+                <Item marca="Ferrari" lancamento={1985}/>
+                <Item marca="Fiat" lancamento={1983}/>
+                <Item marca="Wolksvagen" lancamento={1999}/>
+                <Item  lancamento={1999}/>
             </ul>
         </>
     )
