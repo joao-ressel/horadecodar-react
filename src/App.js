31ad77662278a1e7efd99863ef433e1fe4159e2d
joao-ressel <joao.resselti@gmail.com>
2023-06-23 08:25:33 -0500

    Entendendo o JSX

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import './App.css';
 function App() {
 
   const name = 'João'
+  const newName = name.toUpperCase()
+
+  function sum(a, b){
+    return a + b
+  }
+
+  const url = 'https://st.depositphotos.com/1010338/2099/i/600/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg'
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <p>Olá {name}</p>
+      <p>Olá {newName}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt='Minha Imagem'/>
     </div>
   );
 }
