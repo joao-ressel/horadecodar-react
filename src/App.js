e14e0a8aa6b3b6f9eef5c4a95552f51b29f51136
joao-ressel <joao.resselti@gmail.com>
2023-06-23 09:13:18 -0500

    Utilizando React fragments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      <List/>
       <Frase/>
       <SayMyName nome="João"/>
       <SayMyName nome={nome}/>
