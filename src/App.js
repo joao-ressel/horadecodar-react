55dd6d5ba7228210aea6085532599ba251655b13
joao-ressel <joao.resselti@gmail.com>
2023-06-23 11:28:01 -0500

    Implementando o React Router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+
+import { Link, BrowserRouter as Router, Routes } from 'react-router-dom';
+import {Route, Switch} from 'react-router'
 import './App.css';
-import SeuNome from './components/SeuNome';
-import Saudacao from './components/Saudacao';
+import Home from './pages/Home';
+import Empresa from './pages/Empresa';
+import Contato from './pages/Contato';
+import NavBar from './components/layout/NavBar';
+import Footer from './components/layout/Footer';
 
 function App() {
 
-
-  const [nome, setNome] = useState()
-
   return (
-    <div className="App">
-      <h1>State Lift</h1>
-     <SeuNome setNome={setNome}/>
-     <Saudacao nome={nome}/>
-    </div>
-  );
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<Home />}/>
+        <Route  path='/empresa' element={<Empresa />}/>
+        <Route  path='/contato' element={<Contato />}/>
+      </Routes>
+      <Footer/>
+    </Router>
+  )
 
 }
 
-export default App;
+export default App
