c124ef3641b6a331da25b9662bb9ebb15c37de73
joao-ressel <joao.resselti@gmail.com>
2023-06-23 10:59:40 -0500

    State Lift

---
+++ src/components/SeuNome.js
@@ -0,0 +1,9 @@
+function SeuNome({setNome}){//passando props via metodo
+    return(
+        <div>
+            <p>Digite o seu nome:</p>
+            <input type="text" placeholder="Qual seu nome?" onChange={(e) =>setNome(e.target.value)}/> 
+        </div>
+    )   
+}
+export default SeuNome
