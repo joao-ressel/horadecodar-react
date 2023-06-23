24239c3909982859cf80687e01f7aa2a5f5f783e
joao-ressel <joao.resselti@gmail.com>
2023-06-23 07:57:04 -0500

    iniciando projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+
+  const name = 'João'
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <p>Olá {name}</p>
+    </div>
+  );
+}
+
+export default App;
