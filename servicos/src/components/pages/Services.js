16728f389cf3d4f40de935e7f12cdc50f136b44d
joao-ressel <joao.resselti@gmail.com>
2023-06-27 15:09:13 -0500

    criando loader

--- servicos/src/components/pages/Services.js
+++ servicos/src/components/pages/Services.js
@@ -4,11 +4,13 @@ import styles from './Services.module.css'
 import LinkButton from "../layout/LinkButton"
 import Container from "../layout/Container"
 import ServiceCard from "../service/ServiceCard"
+import Loading from '../layout/Loading'
 import { useEffect, useState } from "react"
 
 function Services(){
 
     const [services, setServices] = useState([])
+    const [removeLoading, setRemoveLoading] = useState(false)
 
     const location = useLocation()
     let message = ''
@@ -17,7 +19,8 @@ function Services(){
     }
 
     useEffect(()=>{
-        fetch('http://localhost:5000/services', {
+        setTimeout(()=>{//exibir loader
+            fetch('http://localhost:5000/services', {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -26,11 +29,14 @@ function Services(){
         .then((resp)=> resp.json())
         .then((data)=>{
             setServices(data)
+            setRemoveLoading(true)
         })
         .catch((err)=> console.log(err))
+        }, 3000)
     },[])
 
     return (
+        
         <div className={styles.service_container}>
             <div className={styles.title_container}>
                 <h1>Meus Serviços</h1>
@@ -38,16 +44,18 @@ function Services(){
             </div>
             {message && <Message msg={message} type="success"/>}
             <Container customClass="start">
-                {services.length > 0 &&
-                    services.map((service)=>(
-                        <ServiceCard 
-                            id={service.id}
-                            name={service.name}
-                            budget={service.budget}
-                            category={service.category.name}
-                            key={service.id}
-                            />
-                            ))}
+                {services.length > 0 && services.map((service)=>(
+                    <ServiceCard 
+                        id={service.id}
+                        name={service.name}
+                        budget={service.budget}
+                        category={service.category.name}
+                        key={service.id}/>
+                ))}
+                {!removeLoading && <Loading/>}
+                {removeLoading && services.length ===0 &&
+                    <p>Não há serviços cadastrados</p>
+                }
             </Container>
         </div>
         
