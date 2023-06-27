7ce3af9c3bc28eba59913fef82d5f9a8d245bdde
joao-ressel <joao.resselti@gmail.com>
2023-06-27 14:49:26 -0500

    criando dashboard

--- servicos/src/components/pages/Services.js
+++ servicos/src/components/pages/Services.js
@@ -1,7 +1,14 @@
 import { useLocation } from "react-router-dom"
 import Message from "../layout/Message"
+import styles from './Services.module.css'
+import LinkButton from "../layout/LinkButton"
+import Container from "../layout/Container"
+import ServiceCard from "../service/ServiceCard"
+import { useEffect, useState } from "react"
 
 function Services(){
+
+    const [services, setServices] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -9,10 +16,39 @@ function Services(){
         message = location.state.message
     }
 
+    useEffect(()=>{
+        fetch('http://localhost:5000/services', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((resp)=> resp.json())
+        .then((data)=>{
+            setServices(data)
+        })
+        .catch((err)=> console.log(err))
+    },[])
+
     return (
-        <div>
-            <h1>Meus Serviços</h1>
+        <div className={styles.service_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Serviços</h1>
+                <LinkButton to="/newservice" text='Criar Serviço'/>
+            </div>
             {message && <Message msg={message} type="success"/>}
+            <Container customClass="start">
+                {services.length > 0 &&
+                    services.map((service)=>(
+                        <ServiceCard 
+                            id={service.id}
+                            name={service.name}
+                            budget={service.budget}
+                            category={service.category.name}
+                            key={service.id}
+                            />
+                            ))}
+            </Container>
         </div>
         
     )
---
+++ servicos/src/components/service/ServiceCard.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom'
+import styles from './ServiceCard.module.css'
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
+
+function ServiceCard({id, name, budget, category, handleRemove}){
+    return(
+        <div className={styles.service_card}>
+            <h4>{name}</h4>
+            <p><span >Orçamento:</span> R${budget}</p>
+            <p className={styles.category_text}><span className={`${styles[category.toLowerCase()]}`}></span>{category}</p>
+            <div className={styles.service_card_actions}>
+                <Link to="/"><BsPencil/> Editar</Link>
+                <button>
+                    <BsFillTrashFill/>Excluir
+                </button>
+            </div>
+        </div>
+    )
+}
+export default ServiceCard
